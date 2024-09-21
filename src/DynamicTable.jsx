4131a9c74a157629203c1141f71ae7aac04bbac0
Ansh2004P <anshkpatel15@gmail.com>
2024-09-22 03:37:59 +0530

    final

--- src/DynamicTable.jsx
+++ src/DynamicTable.jsx
@@ -45,9 +45,7 @@ const DynamicTable = () => {
   const deleteRow = (index) => {
     setRows((prevRows) => prevRows - 1);
     setRowsData((prevRowsData) => prevRowsData.filter((_, i) => i !== index));
-    setOriginalRowsData((prevRowsData) =>
-      prevRowsData.filter((_, i) => i !== index)
-    ); // Update original rows data
+    setOriginalRowsData((prevRowsData) => prevRowsData.filter((_, i) => i !== index)); // Update original rows data
   };
 
   const addColumn = () => {
@@ -61,9 +59,7 @@ const DynamicTable = () => {
     setColumns((prevColumns) => [...prevColumns, newColumnName]);
     setTypes((prevTypes) => [...prevTypes, newColumnType]);
     setRowsData((prevRowsData) => prevRowsData.map((row) => [...row, []]));
-    setOriginalRowsData((prevRowsData) =>
-      prevRowsData.map((row) => [...row, []])
-    ); // Update original rows data
+    setOriginalRowsData((prevRowsData) => prevRowsData.map((row) => [...row, []])); // Update original rows data
 
     setNewColumnName("");
     setNewColumnType("String");
@@ -136,7 +132,7 @@ const DynamicTable = () => {
   };
 
   return (
-    <div className="container mx-auto p-5 bg-gray-900 text-white">
+    <div className="container mx-auto p-5">
       <h1 className="text-center text-2xl font-bold mb-5">Dynamic Table</h1>
 
       <div className="mt-5">
@@ -145,7 +141,7 @@ const DynamicTable = () => {
           <span className="mr-2">Rows: {rows}</span>
           <button
             onClick={addRow}
-            className="bg-gray-700 text-white p-2 rounded mx-1 hover:bg-gray-600"
+            className="bg-gray-300 text-gray-800 p-2 rounded mx-1"
           >
             Add Row
           </button>
@@ -156,20 +152,20 @@ const DynamicTable = () => {
             type="text"
             value={newColumnName}
             onChange={(e) => setNewColumnName(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white p-2 rounded mr-2"
+            className="border border-gray-300 p-2 rounded mr-2"
             placeholder="Column Name"
           />
           <select
             value={newColumnType}
             onChange={(e) => setNewColumnType(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white rounded p-2 mr-2"
+            className="border border-gray-300 rounded p-2 mr-2"
           >
             <option value="String">String</option>
             <option value="Number">Number</option>
           </select>
           <button
             onClick={addColumn}
-            className="bg-blue-600 text-white p-2 rounded hover:bg-blue-500"
+            className="bg-blue-500 text-white p-2 rounded"
           >
             Add Column
           </button>
@@ -180,7 +176,7 @@ const DynamicTable = () => {
           <select
             value={sortColumn}
             onChange={(e) => setSortColumn(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white p-2 rounded mr-2"
+            className="border border-gray-300 p-2 rounded mr-2"
           >
             <option value="">Select Column</option>
             {columns.map((col, index) =>
@@ -194,7 +190,7 @@ const DynamicTable = () => {
           <select
             value={sortCondition}
             onChange={(e) => setSortCondition(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white p-2 rounded mr-2"
+            className="border border-gray-300 p-2 rounded mr-2"
           >
             <option value="">Select Condition</option>
             <option value="greater">Greater than or equal to</option>
@@ -202,7 +198,7 @@ const DynamicTable = () => {
           </select>
           <button
             onClick={handleSort}
-            className="bg-green-600 text-white p-2 rounded hover:bg-green-500"
+            className="bg-green-500 text-white p-2 rounded"
           >
             Sort Data
           </button>
@@ -213,7 +209,7 @@ const DynamicTable = () => {
           <select
             value={searchColumn}
             onChange={(e) => setSearchColumn(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white p-2 rounded mr-2"
+            className="border border-gray-300 p-2 rounded mr-2"
           >
             <option value="">Select Column</option>
             {columns.map((col, index) => (
@@ -226,67 +222,63 @@ const DynamicTable = () => {
             type="text"
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
-            className="border border-gray-600 bg-gray-800 text-white p-2 rounded mr-2"
+            className="border border-gray-300 p-2 rounded mr-2"
             placeholder="Search Value"
           />
           <button
             onClick={handleSearch}
-            className="bg-yellow-600 text-white p-2 rounded hover:bg-yellow-500"
+            className="bg-blue-500 text-white p-2 rounded"
           >
             Search
           </button>
           <button
             onClick={resetSearch}
-            className="bg-red-600 text-white p-2 rounded ml-2 hover:bg-red-500"
+            className="bg-red-500 text-white p-2 rounded ml-2"
           >
             Reset Search
           </button>
         </div>
 
-        <table className="min-w-full border border-gray-600">
+        <table className="min-w-full border border-gray-300">
           <thead>
             <tr>
-              <th className="border border-gray-600 text-center">#</th>
+              <th className="border border-gray-300 text-center">#</th>
               {columns.map((col, index) => (
-                <th key={index} className="border border-gray-600 text-center">
+                <th key={index} className="border border-gray-300 text-center">
                   {col}
                   <button
                     onClick={() => deleteColumn(index)}
-                    className="bg-red-600 text-white text-sm rounded p-1 ml-2"
+                    className="bg-red-500 text-white text-sm rounded p-1 ml-2"
                   >
                     Delete
                   </button>
                 </th>
               ))}
-              <th className="border border-gray-600 text-center">Actions</th>
+              <th className="border border-gray-300 text-center">Actions</th>
             </tr>
           </thead>
           <tbody>
             {rowsData.length > 0 ? (
               rowsData.map((row, i) => (
-                <tr key={i} className="border border-gray-600">
-                  <td className="border border-gray-600 text-center">
-                    {i + 1}
-                  </td>
+                <tr key={i} className="border border-gray-300">
+                  <td className="border border-gray-300 text-center">{i + 1}</td>
                   {row.map((data, index2) => (
                     <td
                       key={index2}
-                      className="border border-gray-600 text-center"
+                      className="border border-gray-300 text-center"
                     >
                       <input
                         type="text"
                         value={data.join(",")}
-                        onChange={(e) =>
-                          handleChange(e.target.value, i, index2)
-                        }
-                        className="border border-gray-600 bg-gray-800 text-white p-2 w-full text-center"
+                        onChange={(e) => handleChange(e.target.value, i, index2)}
+                        className="border border-gray-300 p-2 w-full text-center"
                       />
                     </td>
                   ))}
                   <td>
                     <button
                       onClick={() => deleteRow(i)}
-                      className="bg-red-600 text-white text-sm rounded p-1 w-full hover:bg-red-500"
+                      className="bg-red-500 text-white text-sm rounded p-1 w-full"
                     >
                       Delete Row
                     </button>
